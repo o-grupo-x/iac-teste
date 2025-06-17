0971252ac818701f66dd966296d88c4445a77268
Gabriel <gabrielkuhnen38@gmail.com>
2025-06-17 10:09:56 -0300

    vai q vai

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "frontend" {
   name               = var.cluster_name
   location           = var.zone
-  initial_node_count = 1    # cria o default pool de 1 nó (100 GB)
+  initial_node_count = 1
 }
 
 resource "google_container_node_pool" "primary" {
@@ -12,12 +12,12 @@ resource "google_container_node_pool" "primary" {
 
   node_config {
     machine_type = "e2-medium"
-    disk_size_gb = 20       # seu pool “primary” de 2×20 GB consome só 40 GB
+    disk_size_gb = 20
   }
 }
 
 resource "google_artifact_registry_repository" "frontend" {
-  location      = "us-central1"
+  location      = "var.region"
   repository_id = "frontend"
   description   = "Frontend container repository"
   format        = "DOCKER"
