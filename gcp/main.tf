f7ef3e58baf191ff4816cc79ee378b38cea77e39
Gabriel <gabrielkuhnen38@gmail.com>
2025-06-17 03:37:39 -0300

    vai q vai

--- gcp/main.tf
+++ gcp/main.tf
@@ -2,19 +2,17 @@
 resource "google_container_cluster" "frontend" {
   name     = var.cluster_name
   location = var.zone
+
   remove_default_node_pool = true
-  initial_node_count       = 0
-}
 
-resource "google_container_node_pool" "primary" {
-  name       = "primary"
-  location   = var.zone
-  cluster    = google_container_cluster.frontend.name
-  node_count = var.node_count
+  node_pool {
+    name               = "primary"
+    initial_node_count = var.node_count
 
-  node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 20
+    node_config {
+      machine_type = "e2-medium"
+      disk_size_gb = 20
+    }
   }
 }
 
