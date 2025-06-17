3f5a3a17a90a48677d11a70a78c65c9d9b7b2c3e
Gabriel <gabrielkuhnen38@gmail.com>
2025-06-17 11:17:59 -0300

    vai q vai

--- aws/cluster/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "iac-state-bucket"
-    key    = "aws/cluster/terraform.tfstate"
-    region = "eu-west-1"
-    encrypt = true
-    lock_table = "iac-state-lock"
-  }
-}
--- aws/network/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "iac-state-bucket"
-    key    = "aws/network/terraform.tfstate"
-    region = "eu-west-1"
-    encrypt = true
-    lock_table = "iac-state-lock"
-  }
-}
